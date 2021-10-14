4b3d3bdcd0650a6d292d0954ff94301f414f2998
MaximTishalovich <kuleya1@yandex.by>
2021-10-14 22:30:30 +0300

    task#02

--- core/src/com/iuw/game/SetScreen.java
+++ core/src/com/iuw/game/SetScreen.java
@@ -7,55 +7,80 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-
-
-
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class SetScreen implements Screen {
     final Process game;
-    private String [] maxarray = {"Green theme", "Blue theme"};
     private Stage stage;
-    private OrthographicCamera camera;
     private Texture img;
-    public SetScreen(final Process game){
+    private OrthographicCamera camera;
+
+    public SetScreen(final Process game) {
         this.game = game;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 600, 800);
 
         img = new Texture("main_theme.jpg");
         stage = new Stage(new ScreenViewport());
-        Slider maxim_slider = new Slider(0f, 100f, 1f, false, Process.gameSkin);
-        SelectBox maxim_box = new SelectBox<String>(Process.gameSkin);
-        Slider maxim_slider2 = new Slider(0f, 100f, 1f, false, Process.gameSkin);
-        SelectBox maxim_box2 = new SelectBox<String>(Process.gameSkin);
-        Label max_label = new Label("label", Process.gameSkin);
 
+        Slider slider_1 = new Slider(0, 100, 1, false, Process.gameSkin);
+        Slider slider_2 = new Slider(0, 100, 1, false, Process.gameSkin);
 
-        maxim_slider.setSize(200, 150);
-        maxim_box.setSize(300, 150);
-        maxim_slider2.setSize(200, 150);
-        max_label.setSize(200, 120);
-        maxim_box2.setSize(300, 150);
+        Button button_1 = new Button(Process.gameSkin);
 
+        SelectBox selectbox_1 = new SelectBox<String>(Process.gameSkin);
+        SelectBox selectbox_2 = new SelectBox<String>(Process.gameSkin);
 
+        String[] text_1 = {"Green", "Blue"};
+        String[] text_2 = {"12", "14", "16", "18", "20", "22"};
+        selectbox_1.setItems(text_1);
+        selectbox_2.setItems(text_2);
 
-        maxim_slider.setPosition(370, 200);
-        maxim_slider2.setPosition(370, 500);
-        maxim_box.setPosition(20, 200);
-        maxim_box2.setPosition(20, 200);
-        max_label.setPosition(23, 323);
+        Label label_1 = new Label("Music", Process.gameSkin);
+        Label label_2 = new Label("Sound", Process.gameSkin);
+        Label label_3 = new Label("Something", Process.gameSkin);
+        Label label_4 = new Label("Chto-to", Process.gameSkin);
+        label_1.setFontScale(2,2);
+        label_2.setFontScale(2,2);
+        label_3.setFontScale(2,2);
+        label_4.setFontScale(2,2);
 
+        slider_1.setSize(210, 100);
+        slider_2.setSize(210, 100);
 
-        stage.addActor(maxim_slider);
-        stage.addActor(maxim_box);
-        stage.addActor(maxim_slider2);
-        stage.addActor(max_label);
-        stage.addActor(maxim_box2);
+        button_1.setSize(250, 100);
+
+        selectbox_1.setSize(210, 100);
+        selectbox_2.setSize(210, 100);
+
+        button_1.setPosition(170, 100);
+
+        slider_1.setPosition(60, 300);
+        slider_2.setPosition(330, 300);
+
+        selectbox_1.setPosition(60, 500);
+        selectbox_2.setPosition(330, 500);
+
+        label_1.setPosition(130,600);
+        label_2.setPosition(130,400);
+        label_3.setPosition(380,600);
+        label_4.setPosition(380,400);
+
+        stage.addActor(slider_1);
+        stage.addActor(slider_2);
+
+        stage.addActor(label_1);
+        stage.addActor(label_2);
+        stage.addActor(label_3);
+        stage.addActor(label_4);
+
+        stage.addActor(selectbox_1);
+        stage.addActor(selectbox_2);
+
+        stage.addActor(button_1);
+
 
     }
 
@@ -72,16 +97,16 @@ public class SetScreen implements Screen {
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
-        //game.batch.begin();
-        //game.batch.draw(img, 0, 0);
-        //game.batch.end();
-        //stage.act();
-        //stage.draw();
         game.batch.begin();
+        game.batch.draw(img, 0, 0);
         game.batch.end();
         stage.act();
         stage.draw();
-
+        // game.button3.getClickListener().touchUp();
+//        if (Gdx.input.isTouched()) {
+//            game.setScreen(new ConfigScreen(game));
+//            dispose();
+//        }
     }
 
     @Override
@@ -106,6 +131,6 @@ public class SetScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
     }
 }
