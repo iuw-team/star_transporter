6fe2e46e61e7069841f8353ef320981dc981f085
Paval-from-Belarus <star-war-sandakan@mail.ru>
2021-10-17 16:06:42 +0300

    New updates for sonarcloud

--- core/src/com/iuw/game/MainPlayScreen.java
+++ core/src/com/iuw/game/MainPlayScreen.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -17,29 +16,24 @@ public class MainPlayScreen implements Screen {
     final  Process game;
     private Stage stage;
     private OrthographicCamera camera;
-    private Sound ship_sound_1, ship_sound_2, ship_sound_3;
+    private final Sound ship_sound_1, ship_sound_2, ship_sound_3;
     Texture ship;
     private Rectangle ship_box;
     private long ship_sound_time;
-    final Integer ship_size = 50;
-    private Integer ship_speed, current_speed;
+    final float ship_size = 50;
+    private Integer ship_speed;
     private float angle;
-    private long mass;
-    private float g;
-    private TextureRegion temp_t_region;
+    private final TextureRegion temp_t_region;
 
     public MainPlayScreen(final Process game, Integer ship_speed){
         this.game = game;
         this.ship_speed = ship_speed;
-        mass = 10;
-        g= (float) (mass/Math.pow(100,2));
-        current_speed = 0;
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 600, 800);
+        camera.setToOrtho(false, Process.WIDTH, Process.HEIGHT);
         angle = 0;
         ship_box = new Rectangle();
-        ship_box.x =  300;
-        ship_box.y = 400;
+        ship_box.x =  300f;
+        ship_box.y = 400f;
         ship_box.width = ship_size;
         ship_box.height = ship_size;
         ship = new Texture("ship_50.png"); // 150x150
@@ -60,16 +54,16 @@ public class MainPlayScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        ScreenUtils.clear(1, 1, 1f, 1);
-        if(angle > 6.28 || angle < - 6.28) {
-            angle=0;
+        ScreenUtils.clear(1f, 1f, 1f, 1f);
+        if(angle > 6.28f || angle < - 6.28f) {
+            angle=0f;
         }
         game.batch.begin();
-        game.batch.draw(temp_t_region, ship_box.x, ship_box.y, ship_size/2, ship_size/2, 50, 50,1 , 1, angle*180/3.14f);
+        game.batch.draw(temp_t_region, ship_box.x, ship_box.y, ship_size/2f, ship_size/2f, 50, 50,1 , 1, angle*180/3.14f);
         game.batch.end();
-        angle-=0.001;
-        ship_box.x-= mass*Math.sin(angle)*Gdx.graphics.getDeltaTime();
-        ship_box.y+= mass*Math.cos(angle)*Gdx.graphics.getDeltaTime();
+        angle-=0.001f;
+        ship_box.x-= Math.sin(angle)*Gdx.graphics.getDeltaTime();
+        ship_box.y+= Math.cos(angle)*Gdx.graphics.getDeltaTime();
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)|| Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.UP)) {
 			if(TimeUtils.nanoTime() - ship_sound_time > 7e9) {
 				switch (MathUtils.random(1, 3)) {
@@ -86,11 +80,11 @@ public class MainPlayScreen implements Screen {
 				ship_sound_time = TimeUtils.nanoTime();
 			}
 			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-                angle+=0.01;
+                angle+=0.01f;
 
             }
 			if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-                angle-=0.01;
+                angle-=0.01f;
             }
 			if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
                 ship_box.y -= ship_speed *Math.cos(angle)* Gdx.graphics.getDeltaTime();
@@ -101,10 +95,10 @@ public class MainPlayScreen implements Screen {
                 ship_box.x -= ship_speed * Math.sin(angle)* Gdx.graphics.getDeltaTime();
             }
 		}
-		if(ship_box.x < 0) ship_box.x = 0;
-		if(ship_box.x > 600 - ship_size) ship_box.x = 600 - ship_size; //600 ширина экрана, 50 - ширина корабля
-		if(ship_box.y > 800 - ship_size) ship_box.y = 800 - ship_size;
-		if(ship_box.y < 0) ship_box.y = 0;
+		if(ship_box.x < 0) ship_box.x = 0f;
+		if(ship_box.x > 600f - ship_size) ship_box.x = 600f - ship_size; //600 ширина экрана, 50 - ширина корабля
+		if(ship_box.y > 800f - ship_size) ship_box.y = 800f - ship_size;
+		if(ship_box.y < 0) ship_box.y = 0f;
     }
 
     @Override
